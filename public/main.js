7034be4bba069bd561e99a5fedeccd61e9c05dca
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-01-05 04:32:39 -0500

    prevent duplicate exercise names

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 // This function forces the user to fill out the input fields before clicking on submit button.
-function validateForm() {
+function validateForm(str) {
+
     var nameField = document.forms["myForm"]["name"].value;
     if (nameField == "" || nameField == null) {
       alert("Exercise name must be filled out");
@@ -11,4 +12,18 @@ function validateForm() {
       alert("# of Sets must be filled out");
       return false;
     }
+
+    // Checks to see if there is a duplicate exercise name.
+    // This is done by turning the string into an array and then comparing the elements of the array to the input field name.
+    var strArray = str.split(" ");
+    var input = document.getElementById("nameField").value;
+    for (var i = 0; i < strArray.length; i++) {
+        if (strArray[i] == input) {
+            alert("This exercise has already been added");
+            document.getElementById("nameField").value = "";
+            document.getElementById("setsField").value = "";
+            return false;
+        }
+    }
+
 }
--- server.js
+++ server.js
@@ -12,20 +12,21 @@ const connectionString = "mongodb+srv://duser:97cDsT5j@cluster0.q3kwg.mongodb.ne
 const mongoOptions = {useNewUrlParser : true, useUnifiedTopology: true};
 
 MongoClient.connect(connectionString, mongoOptions).then(client => {
+    
     console.log('Connected to Database');
     const db = client.db('workout');
-    const quotesCollection = db.collection('exercises');
+    const collection = db.collection('exercises');
     
     // Passes the data from the database into the index.ejs file.
     app.get('/', (req, res) => {
-        db.collection('exercises').find().toArray().then(results => {
+        collection.find().toArray().then(results => {
             res.render('index.ejs', { exercises: results });
         }).catch(error => console.error(error));
     });
     
     // Creates exercise and adds it into the database.
     app.post('/insert', (req, res) => {
-        quotesCollection.insertOne(req.body).then(result => {
+        collection.insertOne(req.body).then(result => {
             res.redirect('/');
         }).catch(error => console.error(error));
     });
@@ -34,7 +35,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
     app.post('/update', (req, res) => {
         // This if statement checks to see if there is only 1 set. This determines how the table is creates in the index.ejs file.
         if (req.body.sets.length == 1) {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -53,7 +54,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
                 res.redirect('/');
             }).catch(error => console.error(error));
         } else {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -78,7 +79,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
     app.post('/add', (req, res) => {
         // This if statement checks to see if there is only 1 set. Going from 1 set to 2 set will change how the table is created.
         if (req.body.sets.length == 1) {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -98,7 +99,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
                 res.redirect('/');
             }).catch(error => console.error(error));
         } else {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -127,7 +128,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
     app.post('/sub', (req, res) => {
         // This if statement prevents the number of sets to go below 1.
         if (req.body.sets.length == 1) {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -146,7 +147,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
                 res.redirect('/');
             }).catch(error => console.error(error));
         } else {
-            quotesCollection.findOneAndUpdate(
+            collection.findOneAndUpdate(
                 { name: req.body.name },
                 {
                     $set: {
@@ -170,7 +171,7 @@ MongoClient.connect(connectionString, mongoOptions).then(client => {
 
     // Removes the exercise from the database.
     app.post('/remove', (req, res) => {
-        quotesCollection.deleteOne(
+        collection.deleteOne(
             { name: req.body.name }
         ).then(result => {
             res.redirect('/');
